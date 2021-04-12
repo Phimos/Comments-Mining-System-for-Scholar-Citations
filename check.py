38c82405a872b852168c8158e65132707cf1b33b
phimos <yunchong@pku.edu.cn>
2021-04-12 18:06:07 +0800

    def countertable

--- check.py
+++ check.py
@@ -2,15 +2,23 @@ import json
 
 import tableprint as tp
 
-from citeminer.crawlers.collector import PaperCollector
+# from citeminer.crawlers.collector import PaperCollector
 from citeminer.utils.analysis import collect_failed_data
 
-collector = PaperCollector()
+# collector = PaperCollector()
 with open("./citeminer/configs/basic.json") as infile:
     config = json.load(infile)
-collector.init_from_config(config)
+# collector.init_from_config(config)
 
 # collector.report()
 
 
 collect_failed_data(config["metadata_save_dir"])
+
+
+import pandas as pd
+
+
+class CounterTable(pd.DataFrame):
+    def __init__(self, data=None, index=None, columns=None) -> None:
+        super().__init__(data=data, index=index, columns=[])
