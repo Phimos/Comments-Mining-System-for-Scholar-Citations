09879aaeb34190cc58662e5da1cb38959db7ad97
phimos <yunchong@pku.edu.cn>
2021-04-12 18:05:23 +0800

    config paresr

--- citeminer/__init__.py
+++ citeminer/__init__.py
@@ -1,6 +1,21 @@
+import json
+from typing import Dict
+
 import citeminer.crawlers
 import citeminer.pdfparser
 import citeminer.types
 import citeminer.utils
 
 debug = 1
+
+
+def parse_config(config_path: str):
+    assert config_path.endswith(".json")
+
+    with open(config_path) as f:
+        cfg: Dict = json.load(f)
+
+    cfg.setdefault("metadata_save_dir", "./result/metadata")
+    cfg.setdefault("pdf_save_dir", "./result/pdfs")
+
+    cfg.setdefault("timeout", 10)
--- citeminer/jsonparser.py
+++ citeminer/jsonparser.py
@@ -1,1 +1,13 @@
 import json
+
+
+def parse_json_config(config_path: str):
+    assert config_path.endswith(".json")
+    with open(config_path) as f:
+        config = json.load(f)
+    print(config)
+    return config
+
+
+if __name__ == "__main__":
+    a = parse_json_config("basic.json")
