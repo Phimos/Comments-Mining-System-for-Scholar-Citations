a648fabe0acfb3bf8436194b66e825437b4945fb
phimos <yunchong@pku.edu.cn>
2021-04-13 14:12:59 +0800

    delete unused test

--- test_scholar.py
+++
@@ -1,7 +0,0 @@
-from crawlers import scholarly, ProxyGenerator
-import logging
-from crawlers.freeproxy.freeproxy import FreeProxyPool
-
-
-fp = FreeProxyPool(refresh=True, download=True)
-print(fp.next_proxy())
