289a4265ada7ff0ba3a938e268a15d916e705ee0
phimos <yunchong@pku.edu.cn>
2021-04-14 13:47:01 +0800

    ready to run

--- collect_pdf.py
+++ collect_pdf.py
@@ -2,13 +2,14 @@ import json
 
 from citeminer.crawlers.collector import PaperCollector
 
+print("hello")
 collector = PaperCollector()
 with open("./citeminer/configs/test.json") as infile:
     config = json.load(infile)
 collector.init_from_config(config)
-# collector.collect_metadata()
-collector.json_to_pdf("./result/metadata/Bingxuan Wang")
+collector.collect_metadata()
+# collector.json_to_pdf("./result/metadata/Bingxuan Wang")
 # print(config["authors"][0])
 # out = collector.get_author_info(config["authors"][0])
 # collector.scholar_crawler.pprint(out)
-# collector.collect_pdf_files()
+collector.collect_pdf_files()
