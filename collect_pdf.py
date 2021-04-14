fe1aff23c1c371686d4e1e78aba86b6613b61574
phimos <yunchong@pku.edu.cn>
2021-04-14 12:07:04 +0800

    use json to pdf (file will be delete soon)

--- collect_pdf.py
+++ collect_pdf.py
@@ -3,9 +3,12 @@ import json
 from citeminer.crawlers.collector import PaperCollector
 
 collector = PaperCollector()
-with open("./citeminer/configs/basic.json") as infile:
+with open("./citeminer/configs/test.json") as infile:
     config = json.load(infile)
 collector.init_from_config(config)
-
-
-collector.collect_pdf_files()
+# collector.collect_metadata()
+collector.json_to_pdf("./result/metadata/Bingxuan Wang")
+# print(config["authors"][0])
+# out = collector.get_author_info(config["authors"][0])
+# collector.scholar_crawler.pprint(out)
+# collector.collect_pdf_files()
