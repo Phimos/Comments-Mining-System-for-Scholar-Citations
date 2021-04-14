41e3adbbb711d4dd4151d305753966251fd674d1
phimos <yunchong@pku.edu.cn>
2021-04-14 12:35:21 +0800

    delete parse_config func

--- citeminer/__init__.py
+++ citeminer/__init__.py
@@ -7,15 +7,3 @@ import citeminer.types
 import citeminer.utils
 
 debug = 1
-
-
-def parse_config(config_path: str):
-    assert config_path.endswith(".json")
-
-    with open(config_path) as f:
-        cfg: Dict = json.load(f)
-
-    cfg.setdefault("metadata_save_dir", "./result/metadata")
-    cfg.setdefault("pdf_save_dir", "./result/pdfs")
-
-    cfg.setdefault("timeout", 10)
