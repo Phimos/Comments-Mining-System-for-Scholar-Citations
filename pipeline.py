ca48bc01d90d98559b10f8c6d86c08e4dae6d8cb
phimos <yunchong@pku.edu.cn>
2021-04-15 13:11:07 +0800

    whole pipeline

--- pipeline.py
+++ pipeline.py
@@ -4,8 +4,14 @@ from citeminer.crawlers.collector import PaperCollector
 from citeminer.utils import load_json
 
 collector = PaperCollector()
-config = load_json("./citeminer/configs/basic.json")
+config = load_json("./citeminer/configs/test.json")
 collector.init_from_config(config)
-# collector.collect_metadata()
-# collector.collect_pdfs()
+collector.collect_metadata()
+
+collector.collect_aminer_info()
+
+collector.collect_pdfs()
+
+collector.from_pdf_to_txt()
+
 collector.create_summaries()
