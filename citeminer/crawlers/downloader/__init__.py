26e389b4dd02d253d4e9f802bd0d7718e2034c92
phimos <yunchong@pku.edu.cn>
2021-05-02 10:38:48 +0800

    add base downloader

--- citeminer/crawlers/downloader/__init__.py
+++ citeminer/crawlers/downloader/__init__.py
@@ -1,4 +1,15 @@
+from typing import AbstractSet
+
 from .scihub import SciHub
 from .simple import SimpleDownloader
+
+
+class BaseDownlaoder(object):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def download(self, url: str, path: str) -> bool:
+        return False
+
 
 __all__ = ["SciHub", "SimpleDownloader"]
