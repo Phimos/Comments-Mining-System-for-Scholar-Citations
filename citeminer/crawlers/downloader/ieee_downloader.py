7c4249e37eea116a9e36e3e52636021036245126
phimos <yunchong@pku.edu.cn>
2021-05-03 10:26:43 +0800

    add ieee downloader (not used yet)

--- citeminer/crawlers/downloader/ieee_downloader.py
+++ citeminer/crawlers/downloader/ieee_downloader.py
@@ -6,5 +6,32 @@ class IEEEDownloader(object):
     def __init__(self) -> None:
         super().__init__()
 
-    def download(self, url, path):
-        pass
+    def download(self, url: str, path: str) -> bool:
+        if "ieeexplore.ieee.org" not in url:
+            return False
+
+        try:
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, "lxml")
+            real_url = soup.find("iframe").get("src")
+
+            res = requests.get(real_url)
+            if (
+                "application/pdf" in res.headers["Content-Type"]
+                or "application/octet-stream" in res.headers["Content-Type"]
+                or "application/x-download" in res.headers["Content-Type"]
+            ):
+                with open(path, "wb") as f:
+                    f.write(res.content)
+                return True
+            else:
+                return False
+        except:
+            return False
+
+
+if __name__ == "__main__":
+    downloader = IEEEDownloader()
+    downloader.download(
+        "https://ieeexplore.ieee.org/iel7/8782710/8818473/09264716.pdf", "1.pdf"
+    )
