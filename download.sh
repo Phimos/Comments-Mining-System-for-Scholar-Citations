70e898677ca7176bdc94972a6d1ebf800f178843
phimos <yunchong@pku.edu.cn>
2021-05-05 23:37:26 +0800

    add download.sh

---
+++ download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+for i in $(seq 1 200)
+do
+    echo 'start'
+    echo $i
+    pid=$(ps -ef | grep 'pipeline.py' | grep -v "grep" | awk '{print $2}')
+    if [$pid == '']
+    then
+        echo 'no pipeline'
+    else
+        echo $pid
+        kill -9 $pid
+        sleep 6m
+    fi
+    cd /home/ganyunchong/Comments-Mining-System-for-Scholar-Citations
+    python pipeline.py &
+    sleep 4m
+done
