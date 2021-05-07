c366b24cd8cd1e8550e7083845303221729f0ae3
phimos <yunchong@pku.edu.cn>
2021-05-07 17:49:13 +0800

    add wiley downloader (can't use)

--- citeminer/crawlers/downloader/ieee_downloader.py
+++ citeminer/crawlers/downloader/ieee_downloader.py
@@ -39,6 +39,29 @@ class HindawiDownloader(BaseDownlaoder):
         return self.simple_download(real_url, path)
 
 
+class WileyDownloader(BaseDownlaoder):
+    """
+    Can't use
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def download(self, url: str, path: str, **kwargs: Any) -> bool:
+        if "ietresearch.onlinelibrary.wiley.com" not in url:
+            return False
+
+        try:
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, "lxml")
+            real_url = soup.find("iframe", attrs={"id": "pdf-iframe"}).get("src")
+            real_url = "https://ietresearch.onlinelibrary.wiley.com" + real_url
+        except:
+            return False
+
+        return self.simple_download(real_url, path)
+
+
 if __name__ == "__main__":
     downloader = IEEEDownloader()
     downloader.download(
